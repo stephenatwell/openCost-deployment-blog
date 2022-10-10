23b11052ac4f3a593e02697c85819f0ba778ea0a
Stephen Atwell <stephen.atwell@armory.io>
2022-10-10 14:47:07 -0700

    splitting opencost and prometheus in two

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
-kustomize build --enable-helm firstCluster >firstCluster/manifests.yml
+kustomize build --enable-helm opencost >opencost/manifests.yml
 kustomize build --enable-helm prometheus >prometheus/manifests.yml
 kustomize build --enable-helm . >manifests.yml
