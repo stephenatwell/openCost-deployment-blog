e7c83567c2c0089c3033aff4d66fb361962e06f4
Stephen Atwell <78044713+stephenatwell@users.noreply.github.com>
2022-10-11 13:37:14 -0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,2 @@
-kustomize build --enable-helm opencost >opencost/manifests.yml
-kustomize build --enable-helm prometheus >prometheus/manifests.yml
+kustomize build --enable-helm . >manifests.yml
 armory deploy start -f deploy.yml
